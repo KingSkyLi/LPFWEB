44f123462b567d9833975dd9d6cc62b0fbd251e8
KingSkyLi <15566300566@163.com>
2021-03-12 16:41:11 +0800

    fixed docker control

--- utils/docker-control.js
+++ utils/docker-control.js
@@ -1,13 +1,12 @@
 const docker = new require('dockerode')();
 const path = require('path');
-const { get } = require('https');
 
 /**
- * 
+ * 获取所有镜像列表，或通过指定repository:tag，列出指定镜像
  * @param {string} repository repository:tag
  * @returns [err,imagelist]
  */
-async function getImageList(repository = '') {
+async function listImages(repository = '') {
 	let options;
 	console.log(repository)
 	repository ? options = { filters: JSON.stringify({ reference: [repository] }) } : options = { all: true }
@@ -25,28 +24,14 @@ async function getImageList(repository = '') {
 	return [null, res]
 }
 
-
-async function listImage(name) {
-	let [err, res] = await docker.listImages({ all: true }).then(
-		(res) => {
-			return [null, res];
-		},
-		(err) => {
-			return [err, null];
-		}
-	);
-	if (err) {
-		return Promise.reject(err);
-	}
-	if (name) {
-		return res.filter((item) => item.RepoTags.includes(name));
-	}
-	return res;
-}
-
+/**
+ *
+ * 构建image
+ * @param {string} name 必填 repository:tag
+ * @returns
+ */
 async function buidImage(name) {
 	let context = path.resolve(__dirname, '../dockerfiles/' + name);
-	console.log(context);
 	let stream = await docker.buildImage(
 		{
 			context: context,
@@ -54,7 +39,6 @@ async function buidImage(name) {
 		},
 		{ t: name + ':v1' }
 	);
-	let n = 0;
 	let [err, result] = await new Promise((resolve, reject) => {
 		docker.modem.followProgress(stream, (err, res) => {
 			let res_errors = res.filter((item) => item.error);
@@ -69,34 +53,149 @@ async function buidImage(name) {
 		(err) => [err, null]
 	);
 	if (err) {
-		console.log(err);
-		return;
+		return err;
 	}
 	return result;
 }
 
-async function listContainers() {
-	let res = await docker.listContainers({ all: true });
-	return res;
+/**
+ * 列出所有容器，或通过ID列出指定容器
+ *
+ * @param {string} [containerID='']
+ * @returns
+ */
+async function listContainers(containerID = '') {
+	let options;
+	containerID ? options = { all: true, filters: JSON.stringify({ id: [containerID] }) } : options = { all: true }
+	let [err, res] = await docker.listContainers(options).then(res => [null, res], err => [err, null]);
+	if (err) {
+		return [err, null]
+	}
+	return [null, res]
 }
 
-async function deleteContainer(containers) {
-	containers.forEach((item) => {
-		var container = docker.getContainer(item.Id);
+/**
+ * 删容器
+ *
+ * @param {string} [containerID='']
+ * @returns
+ */
+async function removeContainer(containerID) {
+	let container = docker.getContainer(containerID)
+	let result = await new Promise((resolve, reject) => {
+		container.remove(function (err, data) {
+			if (err) {
+				reject(err)
+			}
+			resolve(data)
+		})
+	}).then(res => [null, res], err => [err, null])
+	return result
+}
+
+/**
+ *
+ *
+ * @param {*} imageName 镜像
+ * @param {*} serverport 服务端口
+ * @param {*} clinetport 客户端端口
+ * @returns
+ */
+async function createContainer(imageName, serverport, clinetport) {
+	if (!imageName || !serverport || !clinetport) {
+		return ['imageName、serverport、clinetport不能为空', null]
+	}
+	let result = docker.createContainer({
+		Image: imageName,
+		Cmd: ['/bin/bash', '-c', 'tail -f /etc/nginx/nginx.conf'],
+		ExposedPorts: { '3000/tcp': {}, '80/tcp': {} },
+		HostConfig: {
+			PortBindings: {
+				'3000/tcp': [{
+					HostIp: '0.0.0.0',
+					HostPort: serverport,
+				}],
+				'80/tcp': [{
+					HostIp: '0.0.0.0',
+					HostPort: clinetport,
+				}],
+			}
+		}
+	}).then(res => [null, res.id], err => [err, null])
+	return result
+}
+
+/**
+ * 启动容器
+ *
+ * @param {*} containerId 容器ID
+ * @returns
+ */
+async function startContainer(containerId) {
+	let container = docker.getContainer(containerId)
+	let result = new Promise((resolve, reject) => {
+		container.start((err, data) => {
+			if (err) {
+				reject(err)
+				return
+			}
+			resolve(data)
+		})
+	}).then(res => [null, `容器：${containerId} 启动成功`], err => [err, null])
+	return result
+}
+
+/**
+ * 停止容器
+ *
+ * @param {*} containerId 容器ID
+ * @returns
+ */
+async function stopContainer(containerId) {
+	let container = docker.getContainer(containerId)
+	let result = new Promise((resolve, reject) => {
+		container.stop((err, data) => {
+			if (err) {
+				reject(err)
+				return
+			}
+			resolve(data)
+		})
+	}).then(res => [null, `容器：${containerId} 停止成功`], err => [err, null])
+	return result
+}
+
+/**
+ * 删除容器
+ *
+ * @param {*} containerId 容器ID
+ * @returns
+ */
+async function removeContainer(containerId) {
+	let container = docker.getContainer(containerId)
+	let result = new Promise((resolve, reject) => {
 		container.remove((err, data) => {
-			console.log(err);
-		});
-	});
+			if (err) {
+				reject(err)
+				return
+			}
+			resolve(data)
+		})
+	}).then(res => [null, `容器：${containerId} 删除成功`], err => [err, null])
+	return result
 }
 
-async function createContainer() {
-	docker.run('ubuntu', ['bash', '-c', 'uname -a'], process.stdout, function (err, data, container) {
-		console.log(data.StatusCode);
-	});
-}
 
 async function testFn() {
-	let res = await getImageList()
-	console.log(res)
+	// let res = await createContainer('lpfwebnode:v1', '3000', '30001')
+	let r = await removeContainer('7d3098808748')
+	console.log(r)
+	// removeContainer('1f90a984cced')
+	// removeContainer('154fa318801f')
+	// removeContainer('a64d9aa11fd4')
+	// const [err, [container]] = await listContainers('cf349d2606f4')
+	// const data = await docker.getContainer(container.Id).inspect()
+	// console.log(data)
+	// console.log(container)
 }
 testFn()
